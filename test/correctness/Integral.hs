f551b3f61fd31e191d034fd2ac2a2cf289e1ccd1
wren ng thornton <wren@community.haskell.org>
2012-03-24 22:21:37 -0400

    Updated correctness tests for Data.ByteString.Lex.Integral
    Ignore-this: bacb92b19040d1ee2966a3a48db1f074
    
    darcs-hash:b129c766ac07b48dd66b79ffdd72cc2f58a5bda5

--- test/correctness/Integral.hs
+++ test/correctness/Integral.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
 ----------------------------------------------------------------
---                                                    2012.01.26
+--                                                    2012.03.24
 -- |
 -- Copyright   :  Copyright (c) 2010--2012 wren ng thornton
 -- License     :  BSD3
@@ -14,6 +14,7 @@ import qualified Data.ByteString              as BS
 import qualified Data.ByteString.Char8        as BS8
 import           Data.ByteString.Lex.Integral
 import           Data.Int
+import           Control.Monad                ((<=<))
 import qualified Test.QuickCheck              as QC
 import qualified Test.SmallCheck              as SC
 
@@ -23,8 +24,8 @@ import qualified Test.SmallCheck              as SC
 -- | Converting a non-negative number to a string using 'show' and
 -- then reading it back using 'readDecimal' returns the original
 -- number.
-prop_readShowIdempotent_unsigned :: (Show a, Integral a) => a -> Bool
-prop_readShowIdempotent_unsigned x =
+prop_readDecimal_show :: (Show a, Integral a) => a -> Bool
+prop_readDecimal_show x =
     let px = abs x
     in  Just (px, BS.empty) == (readDecimal . BS8.pack . show) px
 
@@ -32,16 +33,43 @@ prop_readShowIdempotent_unsigned x =
 -- | Converting a number to a string using 'show' and then reading
 -- it back using @'readSigned' 'readDecimal'@ returns the original
 -- number.
-prop_readShowIdempotent_signed :: (Show a, Integral a) => a -> Bool
-prop_readShowIdempotent_signed x =
+prop_readSignedDecimal_show :: (Show a, Integral a) => a -> Bool
+prop_readSignedDecimal_show x =
     Just (x, BS.empty) == (readSigned readDecimal . BS8.pack . show) x
 
 
+-- | Converting a non-negative number to a string using 'show' and
+-- then reading it back using 'readDecimal_' returns the original
+-- number.
+prop_readDecimalzu_show :: (Show a, Integral a) => a -> Bool
+prop_readDecimalzu_show x =
+    let px = abs x
+    in  px == (readDecimal_ . BS8.pack . show) px
+
+
 -- | Converting a non-negative number to a bytestring using 'packDecimal' and then reading it back using 'read' returns the original number.
-prop_showReadIdempotent_unsigned :: (Read a, Integral a) => a -> Bool
-prop_showReadIdempotent_unsigned x =
+prop_read_packDecimal :: (Read a, Integral a) => a -> Bool
+prop_read_packDecimal x =
     let px = abs x
     in  px == (read . maybe "" BS8.unpack . packDecimal) px
+
+----------------------------------------------------------------
+-- | Converting a non-negative number to a string using 'packHexadecimal' and then reading it back using 'readHexadecimal' returns the original number.
+prop_readHexadecimal_packHexadecimal :: (Show a, Integral a) => a -> Bool
+prop_readHexadecimal_packHexadecimal x =
+    let px = abs x
+    in  Just (px, BS.empty) == (readHexadecimal <=< packHexadecimal) px
+
+-- TODO: how can we check the other composition with QC/SC?
+
+----------------------------------------------------------------
+-- | Converting a non-negative number to a string using 'packOctal' and then reading it back using 'readOctal' returns the original number.
+prop_readOctal_packOctal :: (Show a, Integral a) => a -> Bool
+prop_readOctal_packOctal x =
+    let px = abs x
+    in  Just (px, BS.empty) == (readOctal <=< packOctal) px
+
+-- TODO: how can we check the other composition with QC/SC?
 
 ----------------------------------------------------------------
 {-
@@ -66,34 +94,61 @@ packDecimal = start
 main :: IO ()
 main = do
     putStrLn "QuickCheck tests."
-    putStrLn "Checking prop_readShowIdempotent_unsigned..."
-    QC.quickCheck (prop_readShowIdempotent_unsigned :: Int -> Bool)
-    QC.quickCheck (prop_readShowIdempotent_unsigned :: Int64 -> Bool)
-    QC.quickCheck (prop_readShowIdempotent_unsigned :: Integer -> Bool)
+    putStrLn "Checking prop_readDecimal_show..."
+    QC.quickCheck (prop_readDecimal_show :: Int -> Bool)
+    QC.quickCheck (prop_readDecimal_show :: Int64 -> Bool)
+    QC.quickCheck (prop_readDecimal_show :: Integer -> Bool)
     --
-    putStrLn "Checking prop_readShowIdempotent_signed..."
-    QC.quickCheck (prop_readShowIdempotent_signed :: Int -> Bool)
-    QC.quickCheck (prop_readShowIdempotent_signed :: Int64 -> Bool)
-    QC.quickCheck (prop_readShowIdempotent_signed :: Integer -> Bool)
+    putStrLn "Checking prop_readDecimalzu_show..."
+    QC.quickCheck (prop_readDecimalzu_show :: Int -> Bool)
+    QC.quickCheck (prop_readDecimalzu_show :: Int64 -> Bool)
+    QC.quickCheck (prop_readDecimalzu_show :: Integer -> Bool)
     --
-    putStrLn "Checking prop_showReadIdempotent_unsigned..."
-    QC.quickCheck (prop_showReadIdempotent_unsigned :: Int -> Bool)
-    QC.quickCheck (prop_showReadIdempotent_unsigned :: Int64 -> Bool)
-    QC.quickCheck (prop_showReadIdempotent_unsigned :: Integer -> Bool)
+    putStrLn "Checking prop_readSignedDecimal_show..."
+    QC.quickCheck (prop_readSignedDecimal_show :: Int -> Bool)
+    QC.quickCheck (prop_readSignedDecimal_show :: Int64 -> Bool)
+    QC.quickCheck (prop_readSignedDecimal_show :: Integer -> Bool)
+    --
+    putStrLn "Checking prop_read_packDecimal..."
+    QC.quickCheck (prop_read_packDecimal :: Int -> Bool)
+    QC.quickCheck (prop_read_packDecimal :: Int64 -> Bool)
+    QC.quickCheck (prop_read_packDecimal :: Integer -> Bool)
+    --
+    putStrLn "Checking prop_readHexadecimal_packHexadecimal..."
+    QC.quickCheck (prop_readHexadecimal_packHexadecimal :: Int -> Bool)
+    QC.quickCheck (prop_readHexadecimal_packHexadecimal :: Int64 -> Bool)
+    QC.quickCheck (prop_readHexadecimal_packHexadecimal :: Integer -> Bool)
+    --
+    putStrLn "Checking prop_readOctal_packOctal..."
+    QC.quickCheck (prop_readOctal_packOctal :: Int -> Bool)
+    QC.quickCheck (prop_readOctal_packOctal :: Int64 -> Bool)
+    QC.quickCheck (prop_readOctal_packOctal :: Integer -> Bool)
     --
     putStrLn "SmallCheck tests."
     let d = 2 ^ (8 :: Int) :: Int
-    putStrLn "Checking prop_readShowIdempotent_unsigned..."
-    SC.smallCheck d (prop_readShowIdempotent_unsigned :: Int -> Bool)
-    SC.smallCheck d (intBoundary prop_readShowIdempotent_unsigned)
+    putStrLn "Checking prop_readDecimal_show..."
+    SC.smallCheck d (prop_readDecimal_show :: Int -> Bool)
+    SC.smallCheck d (intBoundary prop_readDecimal_show)
     --
-    putStrLn "Checking prop_readShowIdempotent_signed..."
-    SC.smallCheck d (prop_readShowIdempotent_signed :: Int -> Bool)
-    SC.smallCheck d (intBoundary prop_readShowIdempotent_signed)
+    putStrLn "Checking prop_readDecimalzu_show..."
+    SC.smallCheck d (prop_readDecimalzu_show :: Int -> Bool)
+    SC.smallCheck d (intBoundary prop_readDecimalzu_show)
     --
-    putStrLn "Checking prop_showReadIdempotent_unsigned..."
-    SC.smallCheck d (prop_showReadIdempotent_unsigned :: Int -> Bool)
-    SC.smallCheck d (intBoundary prop_showReadIdempotent_unsigned)
+    putStrLn "Checking prop_readSignedDecimal_show..."
+    SC.smallCheck d (prop_readSignedDecimal_show :: Int -> Bool)
+    SC.smallCheck d (intBoundary prop_readSignedDecimal_show)
+    --
+    putStrLn "Checking prop_read_packDecimal..."
+    SC.smallCheck d (prop_read_packDecimal :: Int -> Bool)
+    SC.smallCheck d (intBoundary prop_read_packDecimal)
+    --
+    putStrLn "Checking prop_readHexadecimal_packHexadecimal..."
+    SC.smallCheck d (prop_readHexadecimal_packHexadecimal :: Int -> Bool)
+    SC.smallCheck d (intBoundary prop_readHexadecimal_packHexadecimal)
+    --
+    putStrLn "Checking prop_readOctal_packOctal..."
+    SC.smallCheck d (prop_readOctal_packOctal :: Int -> Bool)
+    SC.smallCheck d (intBoundary prop_readOctal_packOctal)
     where
     -- | Test 'Integers' around the 'Int' boundary
     intBoundary :: (Integer -> Bool) -> Integer -> Bool
